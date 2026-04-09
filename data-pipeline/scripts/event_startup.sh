50ec884c2fb863c32da1d2695caefa8a41f239b9
Duy Thai <calvados224@gmail.com>
2026-04-09 19:16:30 +0000

    kafka config docker-compose

--- data-pipeline/scripts/event_startup.sh
+++ data-pipeline/scripts/event_startup.sh
@@ -11,19 +11,20 @@ echo "Running Eventsim in detached mode..."
 docker run -itd \
   --network host \
   --name million_events \
-  --memory="8g" \
+  --memory="5.5g" \
+  --memory-swap="8g" \
   --oom-kill-disable \
   events:1.0 \
-    -c "examples/example-config.json" \
-    --start-time "2026-03-01T00:00:00" \
-    --end-time "2026-04-04T23:59:59" \
-    --nusers 200000 \
+    -c "examples/ad-config.json" \
+    --start-time "2026-04-01T00:00:00" \
+    --end-time "2026-05-15T23:59:59" \
+    --nusers 800000 \
     --growth-rate 5 \
     --userid 1 \
     --kafkaBrokerList localhost:9092 \
     --randomseed 42 \
     --continuous
 
-echo "Started streaming events for 100,000 users..."
+echo "Started streaming events for 800,000 users..."
 echo "Eventsim is running in detached mode. "
 echo "Run 'docker logs --follow million_events' to see the logs."
